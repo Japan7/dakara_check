f0cd295ccfcc927a97a6c566bf5cbb9d5d11a351
odrling <florianbadie@odrling.xyz>
2024-04-01 15:17:57 +0200

    ci: lto/pie builds

--- ci/build.sh
+++ ci/build.sh
@@ -5,7 +5,7 @@ if [ "$ARCH" != x86_64 ]; then
     extra_args="--cross-file ci/$ARCH-chimera-linux-musl.txt"
 fi
 
-meson setup build --buildtype release --strip -Ddefault_library=static -Dprefer_static=true -Dffmpeg:programs=disabled -Dffmpeg:tests=disabled -Dffmpeg:encoders=disabled -Dffmpeg:muxers=disabled -Dffmpeg:avfilter=disabled -Dffmpeg:avdevice=disabled -Dffmpeg:postproc=disabled -Dffmpeg:swresample=disabled -Dffmpeg:swscale=disabled -Dffmpeg:decoders=disabled -Dffmpeg:aac_decoder=enabled -Dffmpeg:aac_fixed_decoder=enabled -Dffmpeg:aac_latm_decoder=enabled -Dffmpeg:version3=enabled $extra_args
+meson setup build --buildtype release --strip -Db_lto=true -Db_lto_mode=thin -Db_pie=true -Ddefault_library=static -Dprefer_static=true -Dffmpeg:programs=disabled -Dffmpeg:tests=disabled -Dffmpeg:encoders=disabled -Dffmpeg:muxers=disabled -Dffmpeg:avfilter=disabled -Dffmpeg:avdevice=disabled -Dffmpeg:postproc=disabled -Dffmpeg:swresample=disabled -Dffmpeg:swscale=disabled -Dffmpeg:decoders=disabled -Dffmpeg:aac_decoder=enabled -Dffmpeg:aac_fixed_decoder=enabled -Dffmpeg:aac_latm_decoder=enabled -Dffmpeg:version3=enabled $extra_args
 
 meson compile -C build
 meson install -C build --destdir=../dest/
