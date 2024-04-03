bd1ca68cb7b25ea010b9bb5c03822c4cec367c21
NextFire <git@yuru.moe>
2024-04-03 11:10:55 -0400

    nix flake

---
+++ flake.nix
@@ -0,0 +1,17 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+  };
+
+  outputs = inputs@{ flake-parts, self, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
+
+      perSystem = { pkgs, system, ... }: {
+        devShells.default = with pkgs; mkShell {
+          packages = [ ffmpeg meson ninja pkg-config ];
+        };
+      };
+    };
+}
